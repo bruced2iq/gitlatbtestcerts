c868d7090ed7d6a8762119c048a7a2d791869945
Bruce Jacobs <88393022+bruced2iq@users.noreply.github.com>
2022-01-20 00:08:33 -0800

    Update 1.create-root-ca.sh

--- 1.create-root-ca.sh
+++ 1.create-root-ca.sh
@@ -7,7 +7,7 @@ mkdir -p certs/ca
 
 # Create your very own Root Certificate Authority
 openssl genrsa \
-  -out certs/ca/${FQDN}.key.pem \
+  -out certs/ca/sales-se.d2iq.cloud.key.pem \
   2048
 
 # Self-sign your Root Certificate Authority
@@ -16,7 +16,7 @@ openssl req \
   -x509 \
   -new \
   -nodes \
-  -key certs/ca/${FQDN}.key.pem \
+  -key certs/ca/sales-se.d2iq.cloud.key.pem \
   -days 9131 \
-  -out certs/ca/${FQDN}.crt.pem \
-  -subj "/C=US/ST=Utah/L=Provo/O=ACME Signing Authority Inc/CN=${FQDN}"
+  -out certs/ca/sales-se.d2iq.cloud.crt.pem \
+  -subj "/C=US/ST=Utah/L=Provo/O=ACME Signing Authority Inc/CN=sales-se.d2iq.cloud"
